562ea7b3d99c74405cf3daa06c8da08aa950f45d
ragnarls08 <ragnarls08@gmail.com>
2016-02-09 19:22:10 +0100

    first

---
+++ EmpiricalTester/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace EmpiricalTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+        }
+    }
+}
